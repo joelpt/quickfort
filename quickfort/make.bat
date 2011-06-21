1a5a60c757484050ece86fa74214febb39ef432d
Joel Thornton <githubpub@joelpt.net>
2011-06-21 12:12:38 -0700

    Made quickfort\make.bat a bit more resilient to issues with release folder being created as a file when something's locked during a previous make.bat run
    --HG--
    branch : trunk

--- quickfort/make.bat
+++ quickfort/make.bat
@@ -1,10 +1,10 @@
 del Quickfort.exe
 "c:\Program Files (x86)\AutoHotkey\Compiler\Ahk2Exe.exe" /in "Quickfort.ahk" /icon "Quickfort.ico"
+del release
 rmdir /s /q release
 mkdir release
 mkdir release\config
 move Quickfort.exe release
 copy Quickfort.ico release
 copy config\options.txt release\config
-copy readme.txt release
 
