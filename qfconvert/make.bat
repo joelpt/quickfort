ce5b4a8bf2e509c8eac13e5f71dc68c0a2b18994
Joel Thornton <github@joelpt.net>
2012-04-25 01:18:35 -0700

    Use fast rmdir when possible. Use -s silent switch on cxfreeze.

--- qfconvert/make.bat
+++ qfconvert/make.bat
@@ -1,16 +1,15 @@
 set pythonexe=c:\lang\Python27\python.exe
 
-rmdir /s /q release 2> nul:
+rmdir /s /q /k release 2> nul: || rmdir /s /q release 2> nul:
 mkdir release
 mkdir release\config
 
 copy config\*.* release\config
 
 :%pythonexe% -OO setup.py py2exe
-call cxfreeze -OO qfconvert.py
+call cxfreeze -OO -s qfconvert.py
 
 copy dist\*.* release
-rmdir /s /q dist
-:rmdir /s /q build
+rmdir /s /q /k dist || rmdir /s /q dist
 del *.pyc 2> nul:
 del *.pyo 2> nul:
